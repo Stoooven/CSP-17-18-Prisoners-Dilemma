cba9952538d1ba6393747ad40379448a5eea4c79
Stoooven <34655189+Stoooven@users.noreply.github.com>
2017-12-21 10:48:13 -0500

    Steven & Tyler

--- team5.py
+++ team5.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Steve&Tyler' # Only 10 chars displayed.
+strategy_name = 'Weinberger-Powell Strategy'
+strategy_description = 'A list of questions must be answered whether or not to collude or betray.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,7 +17,17 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    a=['c','b']
+    b=[
+    if my_score==0:
+        random.choice(a)
+        
+    if my_score < their_score and their_history='cc':
+        return 'b'
+    if my_score > their_score:
+        return 'b'
+    else 
+        return 'c'
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
